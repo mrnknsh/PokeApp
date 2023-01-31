7705f1308d9e6d1215394c09d4ca174d52a7f3c7
Marina Knysh <mrnknsh@gmail.com>
2023-01-31 21:34:38 +0300

    Code optimization of evolution

--- src/views/components/pokemonEvolution/index.js
+++ src/views/components/pokemonEvolution/index.js
@@ -7,34 +7,39 @@ import {Link} from "react-router-dom";
 
 export const PokemonEvolution = ({pokeName}) => {
     const [evolution, setEvolution] = useState([])
-
+    const [isLoading, setIsLoading] = useState(false)
 
     const getEvolution = async (pokeName) => {
         try {
-            const evolutionNames = []
-            const evolutionUrls = []
-            const evolutionIds = []
-            const res = await getPokemon(pokeName)
-            await axios.get(res.data.species.url)
-                .then(response => axios.get(response.data.evolution_chain.url))
-                .then(response => {
-                        evolutionNames.push(response.data.chain.species.name)
-                        if (response.data.chain.evolves_to.length) {
-                            evolutionNames.push(response.data.chain.evolves_to[0].species.name)
-                        }
-                        if (response.data.chain.evolves_to[0].evolves_to.length) {
-                            evolutionNames.push(response.data.chain.evolves_to[0].evolves_to[0].species.name)
-                        }
-                        evolutionNames.forEach(elem => evolutionUrls.push(`${API}/pokemon/${elem}`))
-                    }
-                )
-            await axios.all(evolutionUrls.map(url => axios.get(url)))
-                .then(res => res.forEach(elem => evolutionIds.push(elem.data.id)));
+            setIsLoading(true)
+            let evolutionNames = []
+            let evolutionUrls = []
+            let evolutionIds = []
+
+            const pokemonRes = await getPokemon(pokeName)
+
+            const speciesRes = await axios.get(pokemonRes?.data?.species?.url)
+
+            const evolutionChainRes = await axios.get(speciesRes.data.evolution_chain.url)
+
+            evolutionNames.push(evolutionChainRes?.data?.chain?.species?.name)
+            if (evolutionChainRes?.data?.chain?.evolves_to?.length) {
+                evolutionNames.push(evolutionChainRes?.data?.chain?.evolves_to[0]?.species?.name)
+            }
+            if (evolutionChainRes?.data?.chain?.evolves_to[0]?.evolves_to?.length) {
+                evolutionNames.push(evolutionChainRes?.data?.chain?.evolves_to[0]?.evolves_to[0]?.species?.name)
+            }
+            evolutionNames.forEach(name => evolutionUrls.push(`${API}/pokemon/${name}`))
+
+            const pokemonsIdRes = await axios.all(evolutionUrls.map(url => axios.get(url)))
+            pokemonsIdRes.forEach(id => evolutionIds.push(id.data.id));
             setEvolution(evolutionIds.map((id, index) => ({id, name: evolutionNames[index]})))
-        } catch (error) {
+
+        } catch
+            (error) {
             console.log(error)
         } finally {
-
+            setIsLoading(false)
         }
     }
 
@@ -45,26 +50,28 @@ export const PokemonEvolution = ({pokeName}) => {
 
     return (
         <div className={'evolution-wrapper'}>
-            <h3 id={'start'}>Pokemon's evolution tree</h3>
-            {
+            {isLoading ? <p>Evolution is loading..</p> : <>
+                <h3 id={'start'}>Pokemon's evolution tree</h3>
+                {evolution?.length > 0 &&
                 evolution.map((elem, index) => {
                     return (
-                        <div className={'evolution-item'}>
-                            <p key={index + 1} className={'evolution-step'}>Evolution step {index + 1}</p>
+                        <div className={'evolution-item'} key={elem.id}>
+                            <p className={'evolution-step'}>Evolution step {index + 1}</p>
                             <img
                                 src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${elem.id}.png`}
-                                alt={elem.name} key={elem.id * 1000}/>
+                                alt={elem.name}/>
                             <p>
                                 <Link
                                     to={`/pokemons/${elem.name}`}
-                                    className={pokeName === elem.name ? 'pointer-none' : ''}
-                                >
+                                    className={pokeName === elem.name ? 'pointer-none' : ''}>
                                     {elem.name[0].toUpperCase() + elem.name.slice(1)}
                                 </Link>
                             </p>
                         </div>
                     )
                 })
+                }
+            </>
             }
         </div>
     )
