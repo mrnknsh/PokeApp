2281993bfd8cecb248f915f739abfbcded116118
Marina Knysh <mrnknsh@gmail.com>
2023-03-05 20:09:08 +0300

    PA-3: Updated data-2

--- src/views/components/layout/index.js
+++ src/views/components/layout/index.js
@@ -2,10 +2,10 @@ import {Outlet} from 'react-router-dom'
 import {Menu} from "../menu";
 import '../../../style.scss'
 
-export const Layout = ({onSearchPokemon}) => {
+export const Layout = () => {
     return (
         <div>
-            <Menu onSearchPokemon={onSearchPokemon}/>
+            <Menu/>
             <div className={'mt-100'}>
                 <Outlet/>
             </div>
